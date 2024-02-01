7708d22352dc8d83f6da4fa873020eb0b928afc6
maguitaria <t2glma00@students.oamk.fi>
2024-02-01 16:47:52 +0200

    Create input fields for weight and calories and state variables. INtroduce Lambda function.
    Radio button to be implemented

--- assignment5/app/src/main/java/com/example/walktrhoughcalories/MainActivity.kt
+++ assignment5/app/src/main/java/com/example/walktrhoughcalories/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.walktrhoughcalories
 
 import android.os.Bundle
+import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.walktrhoughcalories.ui.theme.WalktrhoughCaloriesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   CalorieScreen()
                 }
             }
         }
@@ -30,17 +46,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun CalorieScreen() {
+    // state variable
+    var weightInput by remember {
+        mutableStateOf("")
+    }
+    Column (
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Heading(title = stringResource(R.string.calories))
+        WeightField(weightInput = weightInput, onValueChange = {weightInput = it} )
+    }
+}
+@Composable
+fun Heading(title: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = title, fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp, bottom = 16.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WalktrhoughCaloriesTheme {
-        Greeting("Android")
-    }
+fun WeightField(weightInput : String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(value = weightInput,
+        onValueChange = onValueChange,
+        label = { Text(text = "Enter weight")},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth()
+    )
 }
+
